ff0c83e7395034173f9ea31ce02d7a961a675762
MLMecham <141757176+MLMecham@users.noreply.github.com>
2024-09-25 09:56:12 -0600

    Finished most of mission 2

---
+++ mission/mission.js
@@ -0,0 +1,27 @@
+let selectElement = document.querySelector("select");
+selectElement.addEventListener("change", changeTheme)
+
+function changeTheme(event)
+{
+    let value = event.target.value
+
+    if (value === "light")
+    {
+        let bodyElement = document.querySelector("body");
+        bodyElement.removeAttribute("class")
+
+        let imgElement = document.querySelector("img");
+        imgElement.removeAttribute("src")
+        imgElement.setAttribute("src", "images/byui-logo_blue.webp") 
+    }
+
+    else if (value === "dark")
+    {
+        let bodyElement = document.querySelector("body");
+        bodyElement.setAttribute("class", "dark")
+
+        let imgElement = document.querySelector("img");
+        imgElement.removeAttribute("src")
+        imgElement.setAttribute("src", "images/byui-logo_white.png")
+    }
+}
